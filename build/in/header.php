684358b1af2e2393524e92339056203e2277d06d
raphaelfabeni <rapha.fabeni@gmail.com>
2014-11-21 16:17:56 -0200

    Add new draft about 2014 and remove folder build/in

--- build/in/header.php
+++
@@ -1,8 +0,0 @@
-<a href="#conteudo" class="sr-only go-content" tabindex="1" accesskey="1">Ir para conteúdo</a>
-<header class="header" role="banner">
-    <figure class="thumb">
-        <img src="build/img/raphael-fabeni.jpg" alt="Raphael Fabeni visto de frente sorrindo para a foto">
-    </figure>
-    <h1 class="title"><a class="hover" href="/">Raphael Fabeni</a></h1>
-    <h2 class="subtitle">Front-end developer</h2>
-</header>
--- build/in/meta.php
+++
@@ -1,5 +0,0 @@
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="keywords" content="website, css, html, flash, website, design, web developer, São Paulo, Design, Raphael Fabeni, peças, Brasil, site, hotsite">
-<meta name="author" content="Raphael Fabeni">
-
-<link rel="icon" href="favicon.ico">
