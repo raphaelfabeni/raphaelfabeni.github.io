7eb1e7a583280d7c877ce793d08a791455aeb31e
Raphael Fabeni <rapha.fabeni@gmail.com>
2015-07-01 19:15:13 -0300

    Test for travis

--- script.bash
+++ script.bash
@@ -1,3 +1,11 @@
-git checkout master
-git merge develop
-git push origin master
+#!/usr/bin/env bash
+set -ex
+
+branch=$TRAVIS_BRANCH
+
+if [[ "$branch" = "develop" ]]; then
+  git fetch origin master:master
+  git checkout master
+  git merge develop
+  git push "https://${GH_TOKEN}@github.com/raphaelfabeni/raphaelfabeni.github.io"
+fi
