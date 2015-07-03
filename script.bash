4ec3296e3ede119e4ed38f4f0686edaa3bca6d39
Raphael Fabeni <rapha.fabeni@gmail.com>
2015-07-03 00:50:03 -0300

    Tests for travis III

--- script.bash
+++ script.bash
@@ -6,6 +6,7 @@ branch=$TRAVIS_BRANCH
 if [[ "$branch" = "develop" ]]; then
   git config user.email "rapha.fabeni@gmail.com"
   git config user.name "raphaelfabeni"
+  git config --global push.default simple
   git fetch origin master:master
   git checkout master
   git merge develop
