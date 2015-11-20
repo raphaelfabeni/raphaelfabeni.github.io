389735480a21ebdbf06ad2401774972cfbae2ff1
Raphael Fabeni <rapha.fabeni@gmail.com>
2015-11-20 18:29:41 -0200

    Test new script

--- script.bash
+++ script.bash
@@ -4,6 +4,8 @@ set -ex
 branch=$TRAVIS_BRANCH
 
 if [[ "$branch" = "develop" ]]; then
+  mv .gitignore __gitignore
+  mv _gitignore .gitignore
   git config user.email "rapha.fabeni@gmail.com"
   git config user.name "raphaelfabeni"
   git config --global push.default simple
@@ -13,4 +15,6 @@ if [[ "$branch" = "develop" ]]; then
   git checkout master
   git merge develop
   git push "https://${GH_TOKEN}@github.com/raphaelfabeni/raphaelfabeni.github.io"
+  mv .gitignore _gitignore
+  mv __gitignore .gitignore
 fi
