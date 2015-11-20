505615af7d41c9edd5aea346905c7d49c8dda877
Raphael Fabeni <rapha.fabeni@gmail.com>
2015-11-20 20:01:11 -0200

    Test scripts VI

--- script.bash
+++ script.bash
@@ -1,16 +1,3 @@
-#!/usr/bin/env bash
-set -ex
-
-branch=$TRAVIS_BRANCH
-
-if [[ "$branch" = "develop" ]]; then
-  git config user.email "rapha.fabeni@gmail.com"
-  git config user.name "raphaelfabeni"
-  git config --global push.default simple
-  git fetch origin master:master
-  git checkout master
-  git merge develop
-  git add -A
-  git commit -m "Travis #$TRAVIS_BUILD_NUMBER"
-  git push "https://${GH_TOKEN}@github.com/raphaelfabeni/raphaelfabeni.github.io"
-fi
+git add -A
+git commit -m "Travis #$TRAVIS_BUILD_NUMBER"
+git push "https://${GH_TOKEN}@github.com/raphaelfabeni/raphaelfabeni.github.io"
---
+++ start.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -ex
+
+branch=$TRAVIS_BRANCH
+
+if [[ "$branch" = "develop" ]]; then
+  git config user.email "rapha.fabeni@gmail.com"
+  git config user.name "raphaelfabeni"
+  git config --global push.default simple
+  git fetch origin master:master
+  git checkout master
+  git merge develop
+fi
