e40d36b4e9145c619d61147b0bd82a060876185c
Raphael Fabeni <rapha.fabeni@gmail.com>
2015-11-22 18:30:18 -0200

    Test II

--- start.bash
+++ start.bash
@@ -8,7 +8,7 @@ if [[ "$branch" = "develop" ]]; then
   git config user.name "raphaelfabeni"
   git config --global push.default simple
   git fetch origin master:master
-  git rebase origin/master
+  git rebase master
   git push -f origin/develop
   git checkout master
   git merge develop
