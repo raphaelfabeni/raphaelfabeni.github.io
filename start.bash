e4ee1de9dda2788fe7b4af204ef6a002886a257d
Raphael Fabeni <rapha.fabeni@gmail.com>
2015-11-22 18:34:53 -0200

    Text V

--- start.bash
+++ start.bash
@@ -7,10 +7,6 @@ if [[ "$branch" = "develop" ]]; then
   git config user.email "rapha.fabeni@gmail.com"
   git config user.name "raphaelfabeni"
   git config --global push.default simple
-  git fetch origin master:master
-  git fetch origin develop:develop
-  git rebase master
-  git push develop HEAD:develop
   git checkout master
   git merge develop
   rm .gitignore
