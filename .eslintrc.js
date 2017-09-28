b5f1a269770dc0c81390a43c1bfff40125e4a49b
Raphael Fabeni <rapha.fabeni@gmail.com>
2017-09-27 23:01:35 -0300

    Fix the script js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-    "env": {
-        "browser": true
-    },
+	"env": {
+		"browser": true
+   },
     "extends": "eslint:recommended",
     "rules": {
-        "indent": [ 2, "tab" ],
-        "linebreak-style": [ 2, "unix" ],
-        "quotes": [ 2, "single" ],
-        "semi": [ 2, "always" ],
-        "no-console": 0
+    	"indent": [ 2, "tab" ],
+    	"linebreak-style": [ 2, "unix" ],
+    	"quotes": [ 2, "single" ],
+    	"semi": [ 2, "always" ],
+    	"no-console": 0
     }
 };
--- src/scripts/colors.js
+++ src/scripts/colors.js
@@ -1,13 +1,29 @@
-var Colors = (function(window, document){
+var Colors = (function(){
+
+	// var hold = document.querySelector('[data-hold]')
+	var colorButton = document.querySelector('[data-button-colors]');
+	var lightButton = document.querySelector('[data-button-dark]');
+	var darkButton = document.querySelector('[data-button-light]');
+	var cover = document.querySelector('[data-cover]');
 
 	function init() {
-		console.log('teste', document);
+		bindEvents();
+	}
+
+	function bindEvents() {
+
+		colorButton.addEventListener('click', toggleColor);
+
+	}
+
+	function toggleColor() {
+		cover.classList.toggle('has-color');
 	}
 
 	return {
 		init: init
 	};
 
-})(window, document);
+})();
 
 Colors.init();
