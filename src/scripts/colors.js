4c1c99dd332bc15d9c1e739131a7871a6da5f1c1
Raphael Fabeni <rapha.fabeni@gmail.com>
2017-09-28 20:51:08 -0300

    General adjustments for package

--- src/scripts/colors.js
+++ src/scripts/colors.js
@@ -1,9 +1,9 @@
 var Colors = (function(){
 
 	// var hold = document.querySelector('[data-hold]')
-	var colorButton = document.querySelector('[data-button-colors]');
-	var lightButton = document.querySelector('[data-button-dark]');
-	var darkButton = document.querySelector('[data-button-light]');
+	const colorButton = document.querySelector('[data-button-colors]');
+	//var lightButton = document.querySelector('[data-button-dark]');
+	//var darkButton = document.querySelector('[data-button-light]');
 	var cover = document.querySelector('[data-cover]');
 
 	function init() {
