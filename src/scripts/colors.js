6b33c7b9e3496ac489da21b5eb9779ba026ce433
Raphael Fabeni <rapha.fabeni@gmail.com>
2017-10-01 19:14:35 -0300

    Adds the actions buttons

--- src/scripts/colors.js
+++ src/scripts/colors.js
@@ -1,10 +1,12 @@
 var Colors = (function(){
 
-	const hold = document.querySelector('[data-hold]');
+	const hold = document.getElementsByTagName('body')[0];
 	const colorButton = document.querySelector('[data-button-colors]');
-	const lightButton = document.querySelector('[data-button-dark]');
-	const darkButton = document.querySelector('[data-button-light]');
+	const darkButton = document.querySelector('[data-button-dark]');
+	const lightButton = document.querySelector('[data-button-light]');
 	const cover = document.querySelector('[data-cover]');
+	const buttons = document.querySelectorAll('[data-button]');
+	const themes = [...document.querySelectorAll('[data-theme]')];
 
 	function init() {
 		bindEvents();
@@ -15,24 +17,44 @@ var Colors = (function(){
 		colorButton.addEventListener('click', () => toggleCoverColor(true));
 		darkButton.addEventListener('click', () => toggleCoverColor(false));
 
-		lightButton.addEventListener('click', () => {
-			toggleTheme();
+		lightButton.addEventListener('click', (event) => {
+			controlStatusButtons(event);
+			controlThemes('light');
 			toggleCoverColor(true);
+		});
+
+		darkButton.addEventListener('click', (event) => {
+			controlStatusButtons(event);
+			controlThemes();
+			toggleCoverColor();
 		});
 
 	}
 
-	function toggleTheme() {
-		hold.classList.toggle('is-light');
+	// Control status for themes buttons
+	function controlStatusButtons() {
+		buttons.forEach(item => item.classList.remove('is-active'));
+		event.target.classList.add('is-active');
 	}
 
+	// Control themes wrap
+	function controlThemes(isLight) {
+		if(isLight) {
+			hold.classList.add('is-light');
+			themes[0].classList.add('is-active');
+		} else {
+			hold.classList.remove('is-light');
+			themes[0].classList.remove('is-active');
+		}
+	}
+
+	// Control status of cover
 	function toggleCoverColor(condition) {
 		if(condition) {
 			cover.classList.add('has-color');
 		} else {
 			cover.classList.remove('has-color');
 		}
-
 	}
 
 	return {
