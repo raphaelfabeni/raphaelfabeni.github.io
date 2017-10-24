3b728598c46a0ad0b6f24815b0600f14ca002a3c
Raphael Fabeni <rapha.fabeni@gmail.com>
2017-10-23 22:25:53 -0200

    Test

--- start.bash
+++ start.bash
@@ -3,8 +3,9 @@ set -ex
 
 branch=$TRAVIS_BRANCH
 
-if [[ "$branch" = "master" ]]; then
+if [[ "$branch" = "develop" ]]; then
   git config user.email "rapha.fabeni@gmail.com"
   git config user.name "raphaelfabeni"
   git config --global push.default simple
+  git checkout -b master develop
 fi
