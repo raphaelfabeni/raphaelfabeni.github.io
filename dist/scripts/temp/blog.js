d42058ab7aca8d79e174cb0465aaa3e631fdf950
Raphael Fabeni <rapha.fabeni@gmail.com>
2021-06-14 21:10:01 -0400

    Fixes categories

--- dist/scripts/temp/blog.js
+++ dist/scripts/temp/blog.js
@@ -1,1 +1,14 @@
-"use strict";
+'use strict';
+
+(function () {
+	var categoryButton = document.querySelector('[data-categories-button]');
+	if (!categoryButton) {
+		return;
+	}
+
+	var categoriesList = document.querySelector('[data-categories-list]');
+
+	categoryButton.addEventListener('click', function () {
+		categoriesList.classList.toggle('is-visible');
+	});
+})();
--- src/scripts/blog.js
+++ src/scripts/blog.js
@@ -1,1 +1,13 @@
+(function() {
+	const categoryButton = document.querySelector('[data-categories-button]');
+	if(!categoryButton) {
+		return;
+	}
 
+
+	const categoriesList = document.querySelector('[data-categories-list]');
+
+	categoryButton.addEventListener('click', function() {
+		categoriesList.classList.toggle('is-visible');
+	});
+})();
